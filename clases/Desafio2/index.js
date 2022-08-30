be137584587766a2d4b320c4e6c48a4db0b2f80e
vic <vicomarin26@gmail.com>
2022-08-30 18:44:25 -0500

    Se crea desafío simulador interactivo

---
+++ clases/Desafio2/index.js
@@ -0,0 +1,31 @@
+    function sumar() {
+        let num1, num2, res;
+        num1 = parseInt(document.calculadora.valor1.value);
+        num2 = parseInt(document.calculadora.valor2.value);
+        res = num1 + num2;
+        alert("el resultado es de " + res);
+    }
+
+    function restar() {
+        let num1, num2, res;
+        num1 = parseInt(document.calculadora.valor1.value);
+        num2 = parseInt(document.calculadora.valor2.value);
+        res = num1 - num2;
+        alert("el resultado es de " + res);
+    }
+
+    function multiplicar() {
+        let num1, num2, res;
+        num1 = parseInt(document.calculadora.valor1.value);
+        num2 = parseInt(document.calculadora.valor2.value);
+        res = num1 * num2;
+        alert("el resultado es de " + res);
+    }
+
+    function dividir() {
+        let num1, num2, res;
+        num1 = parseInt(document.calculadora.valor1.value);
+        num2 = parseInt(document.calculadora.valor2.value);
+        res = num1 / num2;
+        alert("el resultado es de " + res);
+    }
